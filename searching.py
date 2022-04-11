51c82d0fce28fe2a02caae53db12864f3f32844b
vilicek <user@user-MacBook-Pro.local>
2022-04-11 08:52:22 +0200

    Nejake veci dobre

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,10 +13,17 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as jfile:
+        data = json.load(jfile)
+    if field not in set(data.keys()):
+        return None
+    return data[field]
+
 
 
 def main():
-    pass
+    seq_data = read_data("sequential.json", "unordered_numbers")
+    print(seq_data)
 
 
 if __name__ == '__main__':
